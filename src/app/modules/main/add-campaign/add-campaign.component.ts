f1a5305febce26b88415d0fe08683a6c1e7d4cda
gem-ayush <ayush.tiwary@geminisolutions.com>
2022-08-03 16:46:43 +0530

    attachment bug update

--- src/app/modules/main/add-campaign/add-campaign.component.ts
+++ src/app/modules/main/add-campaign/add-campaign.component.ts
@@ -85,7 +85,7 @@ testFINAL=this.sanitized.bypassSecurityTrustHtml(this.testhtml)
   addnote:['',Validators.required],
   footer:['',Validators.required],
   radio2:[''||'false'],
-  fileattach:[''||'attachment.pdf']
+  fileattach:[''||'attachment']
 });
   }
   
