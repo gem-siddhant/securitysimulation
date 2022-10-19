23894940a53c2850472a23e852496ee2f6f2ec0c
gem-ayush <ayush.tiwary@geminisolutions.com>
2022-10-19 17:28:03 +0530

    inactivity logout revert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { Router } from '@angular/router';
-import { BnNgIdleService } from 'bn-ng-idle';
-import { ToastrService } from 'ngx-toastr';
-import { InfomodalComponent } from './shared/infomodal/infomodal.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,25 +7,4 @@ import { InfomodalComponent } from './shared/infomodal/infomodal.component';
 })
 export class AppComponent {
   title = 'gift-project';
-
-  constructor (private bnIdle: BnNgIdleService,private router:Router,private dialog:MatDialog, private toastr:ToastrService)
-  {
-
-  }
-  ngOnInit(): void {
-    //60 = 1 minute
-    this.bnIdle.startWatching(1200).subscribe((res) => {
-      if (res && localStorage.getItem("email") !== null) {
-        console.log('session expired');
-        let dataDialog = { title: 'Your session is timed out please login again' };
-        const dialogRef = this.dialog.open(InfomodalComponent, {
-          width: '400px',
-          height:'380px',
-          data: dataDialog
-        });
-        localStorage.clear();
-        this.router.navigate(['main/login']);
-      }
-    });
-  }
 }
