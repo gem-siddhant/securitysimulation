f22ed5237be10ba0e84fe216e0cab58f79a69c07
gem-ayush <ayush.tiwary@geminisolutions.com>
2023-03-16 14:14:40 +0530

    ip record

--- src/app/modules/main/error-page/error-page.component.ts
+++ src/app/modules/main/error-page/error-page.component.ts
@@ -1,32 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { ThemePalette } from '@angular/material/core';
-import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { FormdataService } from '../service/formdata.service';
-
+import { MainService } from '../service/main.service';
 @Component({
   selector: 'app-error-page',
   templateUrl: './error-page.component.html',
   styleUrls: ['./error-page.component.css']
 })
 export class ErrorPageComponent implements OnInit {
+  email:any;
+  ipAddress: any;
+  id:any;
+  loginForm:FormGroup;
+  loginsso:boolean;
 
-  constructor(  private shared: FormdataService) { }
-  StoreData:boolean=true;
-  color: ThemePalette = 'primary';
-  value = 50;
-  mode: ProgressSpinnerMode = 'indeterminate';
-  sso: boolean;
-  signin: boolean;
-  async ngOnInit(): Promise<void> {
-    const sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
-    await sleep(5000);
-    this.StoreData=false
-    this.sso=this.shared.getclick()
-    if (this.sso==null)
-    {
-      this.sso=true
+  constructor(private route:ActivatedRoute,
+    private shared: FormdataService,
+    private toastr:ToastrService,
+    private http:HttpClient,
+    private router:Router,
+    private _mainService:MainService,private formBuilder: FormBuilder) 
+  {
+    this.loginForm = this.formBuilder.group({});
+   }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(
+      params => {
+        this.email =  params['token'];
+        this.id =  params['vals'];
+      }
+    )
+    this.http.get<{ip:string}>('https://jsonip.com')
+    .subscribe( data => {
+      this.ipAddress = data.ip
+      this.sendData(this.email,this.ipAddress,this.id);
+    })
+
+  }
+  sendData(emailID:any,ip:any,id:any){
+    let obj={
+      'token':emailID,
+      'ip':ip,
+      'vals':id
     }
-    window.onbeforeunload = function() { return "Your work will be lost."; };
+    this._mainService.sendUserDetails(obj).subscribe((data:any)=>{
+      if(data){
+        
+      }
+    })
   }
 
 }
