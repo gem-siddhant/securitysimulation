f31485572c3c0fd9b30a747cd1b47d5e4d8b4933
Udit Gupta <Udit.Gupta@Geminisolutions.com>
2023-03-28 18:34:11 +0530

    passing encrypted ipaddress in register-click api

--- src/app/modules/main/error-page/error-page.component.ts
+++ src/app/modules/main/error-page/error-page.component.ts
@@ -3,6 +3,7 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { CommonService } from 'src/app/services/common.service';
 import { FormdataService } from '../service/formdata.service';
 import { MainService } from '../service/main.service';
 @Component({
@@ -22,7 +23,8 @@ export class ErrorPageComponent implements OnInit {
     private toastr:ToastrService,
     private http:HttpClient,
     private router:Router,
-    private _mainService:MainService,private formBuilder: FormBuilder) 
+    private _mainService:MainService,private formBuilder: FormBuilder,
+    private commonService : CommonService) 
   {
     this.loginForm = this.formBuilder.group({});
    }
@@ -30,22 +32,22 @@ export class ErrorPageComponent implements OnInit {
   ngOnInit(): void {
     this.route.queryParams.subscribe(
       params => {
-        this.email =  params['token'];
-        this.id =  params['vals'];
+        this.email =  params['v1'];
+        this.id =  params['v2'];
       }
     )
     this.http.get<{ip:string}>('https://jsonip.com')
     .subscribe( data => {
       this.ipAddress = data.ip
-      this.sendData(this.email,this.ipAddress,this.id);
+      this.sendData(this.email,this.commonService.encrypt(this.ipAddress),this.id);
     })
 
   }
   sendData(emailID:any,ip:any,id:any){
     let obj={
-      'token':emailID,
-      'ip':ip,
-      'vals':id
+      'v1':emailID,
+      'v2':id,
+      'v3':ip
     }
     this._mainService.sendUserDetails(obj).subscribe((data:any)=>{
       if(data){
